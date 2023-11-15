06074aefcdf6806d2abeb93b1aebe16400a00bd4
srakuma <robben2159@gmail.com>
2023-11-15 17:01:22 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+set -uex
+
+cd $(dirname $0)
+
+function backup_originals() {
+    if [ ! -d "$HOME"/.backups ]; then
+        mkdir "$HOME"/.backups
+    fi
+    
+    for f in .??*; do
+        [ "$f" = ".git" ] && continue
+        mv ~/"$f" ~/.backups
+    done
+}
+
+function create_link() {
+    for f in .??*; do
+        [ "$f" = ".git" ] && continue
+        ln -snvf $PWD/"$f" ~/
+    done
+}
+
+backup_originals();
+create_link();
+
+git config --global include.path "~/.gitconfig_shared"
+echo "\033[38;5;40m done \033[0m"
