37749889911e6ebd884bf83535a93daea501fc15
sota-rakuma <robben2159@gmail.com>
2025-10-01 18:58:43 +0900

    add: install wezterm

--- install.sh
+++ install.sh
@@ -57,6 +57,15 @@ fi
 
 mise use -g rust
 
+# install wezterm
+curl https://sh.rustup.rs -sSf | sh -s
+curl -LO https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203-110809-5046fc22-src.tar.gz
+tar -xzf wezterm-20240203-110809-5046fc22-src.tar.gz
+cd wezterm-20240203-110809-5046fc22
+./get-deps
+cargo build --release
+cargo run --release --bin wezterm -- start
+
 
 # install delta
 cargo install git-delta
