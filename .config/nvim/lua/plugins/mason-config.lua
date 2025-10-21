bce3561b49e05002174d68fa90120c979fcc986a
sota-rakuma <robben2159@gmail.com>
2025-10-21 14:59:59 +0900

    update: place to display diagnostic message

--- .config/nvim/lua/plugins/mason-config.lua
+++ .config/nvim/lua/plugins/mason-config.lua
@@ -10,3 +10,9 @@ require("mason-lspconfig").setup({
     "rust_analyzer",
   },
 })
+
+-- 診断メッセージを行の下に表示
+vim.diagnostic.config({
+  virtual_text = false,      -- 行末の表示を無効化
+  virtual_lines = true,      -- 行の下に表示（Neovim 0.11以降）
+})
