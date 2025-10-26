b8041361be961cd26208c59faa73eb83c7b3a282
sraku2159 <robben2159@gmail.com>
2025-10-26 13:38:22 +0900

    fix: nvim configuration

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ return {
     event = "LazyFile",
     dependencies = {
       "mason.nvim",
-      { "mason-org/mason-lspconfig.nvim", config = function() end },
+      { "mason-org/mason-lspconfig.nvim", config = require("mason-lspconfig") },
     },
     opts = function()
       ---@class PluginLspOpts
@@ -135,9 +135,9 @@ return {
       if opts.inlay_hints.enabled then
         LazyVim.lsp.on_supports_method("textDocument/inlayHint", function(client, buffer)
           if
-            vim.api.nvim_buf_is_valid(buffer)
-            and vim.bo[buffer].buftype == ""
-            and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buffer].filetype)
+              vim.api.nvim_buf_is_valid(buffer)
+              and vim.bo[buffer].buftype == ""
+              and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buffer].filetype)
           then
             vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
           end
@@ -186,7 +186,7 @@ return {
       local have_mason = LazyVim.has("mason-lspconfig.nvim")
       local mason_all = have_mason
           and vim.tbl_keys(require("mason-lspconfig.mappings").get_mason_map().lspconfig_to_package)
-        or {} --[[ @as string[] ]]
+          or {} --[[ @as string[] ]]
       local mason_exclude = {} ---@type string[]
 
       ---@return boolean? exclude automatic setup
